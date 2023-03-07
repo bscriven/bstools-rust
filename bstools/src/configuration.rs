c9850e5d97258c9c064d33ac280e8b2a04cde101
bscriven <bscriven@gmail.com>
2023-03-07 13:06:19 -0700

    Refactored

--- bstools/src/configuration.rs
+++ bstools/src/configuration.rs
@@ -1,8 +1,44 @@
+
 use std::env;
+use std::error::Error;
+use std::path::Path;
+
+use crate::configuration;
+use crate::constants;
+use crate::filesystem;
+use crate::runner;
 
 pub fn get_environment_variable(variable_name: &str) -> Option<String> {
     match env::var(variable_name) {
         Ok(value) => Some(value),
         Err(_) => None,
+    }
+}
+
+pub fn verify_environment_configured_and_get_home_path() -> Result<String, Box<dyn Error>> {
+    let home_path_option = configuration::get_environment_variable(constants::ENVIRONMENT_HOME);
+
+    match home_path_option {
+        None => {
+            eprintln!("Mandatory environment variable '{}' does not exist. Set the environment variable and try again.", constants::ENVIRONMENT_HOME);
+            eprintln!("'{}' must contain a directory path. Within the directory, commands and data will be stored.", constants::ENVIRONMENT_HOME);
+            eprintln!("If this is a new installation, an empty directory may be used.");
+
+            return Err(Box::from("Mandatory environment variable does not exist."));
+        }
+        Some(home_path_string) => {
+            let home_path = Path::new(home_path_string.as_str());
+            let data_path = Path::join(home_path, "data");
+
+            filesystem::create_directories(data_path.clone());
+
+            let runners = runner::get_runners(home_path.to_path_buf());
+
+            for runner in &runners {
+                filesystem::create_directories(runner.path.clone());
+            }
+
+            return Ok(home_path_string);
+        }
     }
 }
--- bstools/src/main.rs
+++ bstools/src/main.rs
@@ -11,71 +11,63 @@ use crate::models::FileSystemEntry;
 use crate::models::RunnerCommand;
 
 fn main() {
-    let home_path_option = configuration::get_environment_variable(constants::ENVIRONMENT_HOME);
+    // Ensure that the environment is configured correctly and get the directory path for BS_HOME
+    let environment_verification_result = configuration::verify_environment_configured_and_get_home_path();
 
-    match home_path_option {
-        None => {
-            eprintln!("Mandatory environment variable '{}' does not exist. Set the environment variable and try again.", constants::ENVIRONMENT_HOME);
-            eprintln!("'{}' must contain a directory path. Within the directory, commands and data will be stored.", constants::ENVIRONMENT_HOME);
-            eprintln!("If this is a new installation, an empty directory may be used.");
+    if environment_verification_result.is_err() {
+        process::exit(1);
+    }
 
-            process::exit(1);
+    let home_path_string = environment_verification_result.unwrap();
+    let home_path = Path::new(home_path_string.as_str());
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let runners = runner::get_runners(home_path.to_path_buf());
+    let mut runner_options: Vec<FileSystemEntry> = Vec::new();
+    let mut command: Option<RunnerCommand> = None;
+
+    if args.len() == 0 {
+        // No arguments were provided, so get the options that are available at the root
+        runner_options = runner::get_root_options(runners);
+    }
+    else {
+        // Try to form a command to execute from the arguments that were provided
+        command = runner::get_command(runners.clone(), args.clone());
+
+        if command.is_none() {
+            // If a command cannot be formed, then get the options that are available using the provided arguments
+            let get_options_option = runner::get_options(runners, args.clone());
+            
+            if get_options_option.is_none() {
+                // If no options are available, then the provided arguments are invalid. Display an error and exit.
+                eprintln!("The command you entered is not valid. Enter a valid command and try again.");
+
+                process::exit(2);
+            }
+            else {
+                runner_options = get_options_option.unwrap();
+            }
         }
-        Some(home_path_string) => {
-            let home_path = Path::new(home_path_string.as_str());
-            let data_path = Path::join(home_path, "data");
+    }
 
-            filesystem::create_directories(data_path.clone());
+    if command.is_some() {
+        // If a command is available, then execute it
+        runner::run_command(command.unwrap());
+    }
+    else {
+        // If no command is available, then display the options that are available
+        if runner_options.len() > 0 {
+            println!("Available options:");
 
-            let runners = runner::get_runners(home_path.to_path_buf());
-
-            for runner in &runners {
-                filesystem::create_directories(runner.path.clone());
+            for option in runner_options {
+                println!("    {}", option.name.to_str().unwrap());
             }
-
-            let args: Vec<String> = std::env::args().skip(1).collect();
-            let mut runner_options: Vec<FileSystemEntry> = Vec::new();
-            let mut command: Option<RunnerCommand> = None;
-
-            if args.len() == 0 {
-                runner_options = runner::get_root_options(runners);
-            }
-            else {
-                command = runner::get_command(runners.clone(), args.clone());
-
-                if command.is_none() {
-                    let get_options_option = runner::get_options(runners, args.clone());
-                    
-                    if get_options_option.is_none() {
-                        eprintln!("The command you entered is not valid. Enter a valid command and try again.");
-
-                        process::exit(2);
-                    }
-                    else {
-                        runner_options = get_options_option.unwrap();
-                    }
-                }
-            }
-
-            if command.is_some() {
-                runner::run_command(command.unwrap());
+        }
+        else {
+            if args.len() > 0 {
+                eprintln!("The '{}' directory is empty. Try adding commands to the directory.", args.last().unwrap());
             }
             else {
-                if runner_options.len() > 0 {
-                    println!("Available options:");
-
-                    for option in runner_options {
-                        println!("    {}", option.name.to_str().unwrap());
-                    }
-                }
-                else {
-                    if args.len() > 0 {
-                        eprintln!("The '{}' directory is empty. Try adding commands to the directory.", args.last().unwrap());
-                    }
-                    else {
-                        eprintln!("{} contains no commands. Try adding commands.", home_path_string);
-                    }
-                }
+                eprintln!("{} contains no commands. Try adding commands.", home_path_string);
             }
         }
     }
